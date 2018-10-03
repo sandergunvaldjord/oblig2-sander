820060d1c8632b1d985882b387d037cb20720708
sandergunvaldjord <sanderjg@hiof.no>
2018-10-03 13:25:51 +0200

    Initial commit

---
+++ knapp.js
@@ -0,0 +1,19 @@
+window. onload = oppstart;
+
+function oppstart(){
+
+document.getElementById("btnSøk").onclick = søk;
+
+function søk(){
+
+
+    
+}
+
+}
+<button type="button" button id="btnSøk">Github</button>
+
+
+
+
+
